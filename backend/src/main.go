73fedfc406752115b4d0a20a2f1e7948e9564d8b
Hang Yang <yh_victor@qq.com>
2017-02-22 02:23:06 -0800

    inital golang

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+func printError(w http.ResponseWriter, status int) {
+	fmt.Fprintf(w, "{status: %d}", status)
+}
+
+func writeBack(w http.ResponseWriter, jsonBody []byte) {
+	var x interface{}
+	if err := json.Unmarshal(jsonBody, &x); err != nil {
+		printError(w, -1)
+		return
+	}
+
+	json.NewEncoder(w).Encode(x)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	jsonBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		printError(w, -1)
+	}
+
 	type Message struct {
-		Name string
+		Action string
+	}
+
+	var message Message
+	if err = json.Unmarshal(jsonBody, &message); err != nil {
+		printError(w, -1)
+	}
+
+	log.Printf("{action: %s}", message.Action)
+
+	switch message.Action {
+	case "Login":
+		writeBack(w, jsonBody)
+		return
+	default:
+		printError(w, -2)
+		return
 	}
 }
 
